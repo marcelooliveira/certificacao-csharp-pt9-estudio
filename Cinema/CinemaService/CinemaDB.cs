3b31d4226eb56be9c16d2068239bc38ea04b2c46
marcelooliveira <mclricardo@gmail.com>
2018-12-11 17:25:36 -0200

    Cinema

---
+++ Cinema/CinemaService/CinemaDB.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Cinema.Dados
+{
+    public class CinemaDB
+    {
+        private readonly string databaseServer;
+        private readonly string masterDatabase;
+        private readonly string databaseName;
+
+        public CinemaDB(string databaseServer, string masterDatabase, string databaseName)
+        {
+            this.databaseServer = databaseServer;
+            this.masterDatabase = masterDatabase;
+            this.databaseName = databaseName;
+        }
+
+        public async Task CriarBancoDeDadosAsync()
+        {
+            await CriarBancoAsync();
+            await CriarTabelasAsync();
+            await InserirRegistrosAsync();
+        }
+
+        private async Task CriarBancoAsync()
+        {
+            string sql = $@"IF EXISTS (SELECT * FROM sys.databases WHERE name = N'{databaseName}')
+                    BEGIN
+                        DROP DATABASE [{databaseName}]
+                    END;
+                    CREATE DATABASE [{databaseName}];";
+            await ExecutarComandoAsync(sql, masterDatabase);
+        }
+
+        private async Task CriarTabelasAsync()
+        {
+            string sql = $@"CREATE TABLE [dbo].[Diretores] (
+                        [Id]   INT           IDENTITY (1, 1) NOT NULL,
+                        [Nome] VARCHAR (255) NOT NULL
+                    );
+                    CREATE TABLE [dbo].[Filmes] (
+                        [Id]        INT           IDENTITY (1, 1) NOT NULL,
+                        [DiretorId] INT           NOT NULL,
+                        [Titulo]    VARCHAR (255) NOT NULL,
+                        [Ano]       INT           NOT NULL,
+                        [Minutos]   INT           NOT NULL
+                    );";
+            await ExecutarComandoAsync(sql, databaseName);
+        }
+
+        private async Task InserirRegistrosAsync()
+        {
+            string sql = @"
+                    INSERT Diretores (Nome) VALUES ('Quentin Tarantino');
+                    INSERT Diretores (Nome) VALUES ('James Cameron');
+                    INSERT Diretores (Nome) VALUES ('Tim Burton');
+
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (1, 'Pulp Fiction', 1994,	154);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (1, 'Django Livre', 2012,	165);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (1, 'Kill Bill Volume 1', 2003,	111);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (2, 'Avatar', 2009,	162);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (2, 'Titanic', 1997,	194);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (2, 'O Exterminador do Futuro', 1984,	107);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (3, 'O Estranho Mundo de Jack', 1993,	76);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (3, 'Alice no País das Maravilhas', 2010,	108);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (3, 'A Noiva Cadáver', 2005,	77);
+                    INSERT Filmes (DiretorId, Titulo, Ano, Minutos) VALUES (3, 'A Fantástica Fábrica de Chocolate', 2005,	115);";
+            await ExecutarComandoAsync(sql, databaseName);
+        }
+
+        private async Task ExecutarComandoAsync(string sql, string banco)
+        {
+            SqlConnection conexao = new SqlConnection($"Server={databaseServer};Integrated security=SSPI;database={banco}");
+            SqlCommand comando = new SqlCommand(sql, conexao);
+            try
+            {
+                conexao.Open();
+                await comando.ExecuteNonQueryAsync();
+                Console.WriteLine("Script executado com sucesso.");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (conexao.State == ConnectionState.Open)
+                {
+                    conexao.Close();
+                }
+            }
+        }
+
+        public async Task<IList<Filme>> GetFilmes()
+        {
+            IList<Filme> filmes = new List<Filme>();
+            string connectionString = $"Server={databaseServer};Integrated security=SSPI;database={databaseName}";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(
+                    " SELECT d.Nome AS Diretor, f.Titulo AS Titulo" +
+                    " FROM Filmes AS f" +
+                    " INNER JOIN Diretores AS d" +
+                    "   ON d.Id = f.DiretorId"
+                    , connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                while (reader.Read())
+                {
+                    string diretor = reader["Diretor"].ToString();
+                    string titulo = reader["Titulo"].ToString();
+                    filmes.Add(new Filme(diretor, titulo));
+                }
+            }
+
+            return filmes;
+        }
+    }
+}
